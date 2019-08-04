4ba533f787c65e92752fa8609394e6744d3edabe
Vicky Abishek <vicky.abishek@outlook.com>
2019-08-04 22:13:20 +0530

    day - 004
    Raised Button

--- timesheet/lib/view/login.dart
+++ timesheet/lib/view/login.dart
@@ -22,16 +22,27 @@ class LoginPage extends StatelessWidget {
       // color: Colors.cyanAccent,
       padding: EdgeInsets.all(20.0),
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: <Widget>[
-          Container(
-            color: Colors.redAccent,
-            child: SizedBox(
-              height: 100.0,
-              width: 100.0,
-            ),
-            // padding: Padding(padding: ,),
+          // Container(
+          //   color: Colors.redAccent,
+          //   child: SizedBox(
+          //     height: 100.0,
+          //     width: 100.0,
+          //   ),
+          //   // padding: Padding(padding: ,),
+          // )
+          RaisedButton(
+                      child: Text(
+              'Sign in',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontWeight: FontWeight.w600,
+                fontSize: 24.0
+               ),
+            ), onPressed: () {},
           ),
           SizedBox(height: 10.0,),
           Container(
