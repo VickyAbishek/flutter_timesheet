65c5c254d0bf65e84a41ac3b2cc241d0b5b96bfe
Abishek V <Abishek.V@asg.com>
2019-11-28 20:10:49 +0530

    styling button

---
+++ timesheet/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/vickyabishek/Documents/softwares/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/vickyabishek/vicky😎/flutter_timesheet/timesheet"
+export "FLUTTER_TARGET=/Users/vickyabishek/vicky😎/flutter_timesheet/timesheet/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/vickyabishek/Documents/softwares/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
--- timesheet/lib/view/login.dart
+++ timesheet/lib/view/login.dart
@@ -34,39 +34,55 @@ class LoginPage extends StatelessWidget {
           //   ),
           //   // padding: Padding(padding: ,),
           // )
-          RaisedButton(
-                      child: Text(
-              'Sign in',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontWeight: FontWeight.w600,
-                fontSize: 24.0
-               ),
-            ), onPressed: null,
+          // RaisedButton(
+          //             child: Text(
+          //     'Sign in',
+          //     textAlign: TextAlign.center,
+          //     style: TextStyle(
+          //       fontWeight: FontWeight.w600,
+          //       fontSize: 24.0
+          //      ),
+          //   ), onPressed: null,
+          // ),
+          Text(
+            'Sign In',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+                fontStyle: FontStyle.normal,
+                fontSize: 32.0,
+                fontWeight: FontWeight.w500),
           ),
-          SizedBox(height: 10.0,),
-          RaisedButton(
-                      child: Text(
-              'Google Sign in',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontWeight: FontWeight.w600,
-                fontSize: 24.0
-               ),
-            ), onPressed: null,
+          SizedBox(
+            height: 10.0,
           ),
-          SizedBox(height: 10.0,),
           RaisedButton(
-                      child: Text(
-              'Facebook Sign in',
+            child: Text(
+              'Google',
               textAlign: TextAlign.center,
-              style: TextStyle(
-                fontWeight: FontWeight.w600,
-                fontSize: 24.0
-               ),
-            ), onPressed: null,
+              style: TextStyle(fontWeight: FontWeight.w600, fontSize: 22.0),
+            ),
+            onPressed: () {},
+            color: Colors.red[400],
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(16.0))),
           ),
-          SizedBox(height: 10.0,),
+          SizedBox(
+            height: 10.0,
+          ),
+          RaisedButton(
+            color: Colors.blue[400],
+            child: Text(
+              'Facebook',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontWeight: FontWeight.w600, fontSize: 22.0),
+            ),
+            onPressed: () {},
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(16.0))),
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
         ],
       ),
     );
